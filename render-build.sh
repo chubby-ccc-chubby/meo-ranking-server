61236150220457232976a57905b05c60af67aa76
usihgusa <89238104+usihgusa@users.noreply.github.com>
2025-04-17 17:20:10 +0900

    Force add updated render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# キャッシュフォルダと不要ファイルを強制削除
 rm -rf node_modules package-lock.json /opt/render/.cache/puppeteer
-
-# PuppeteerをChromium付きで強制インストール
 PUPPETEER_SKIP_DOWNLOAD=false npm install puppeteer
